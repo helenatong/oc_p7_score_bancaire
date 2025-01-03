d9f576f7abbf037e3ca2b26d2165ed1b9fdc0c62
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-03 17:15:02 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -8,21 +8,11 @@ full_pl = joblib.load(filename="lightgbm_model.joblib")
 
 @app.get("/")
 def home():
-    return {'pipeline_loaded': full_pl}
+    return {'api_availibility': 'OK_model_loaded'}
 
-
-# @app.get("/")
-# def home():
-#     return {'api_availibility': 'OK_test', 'pipeline_loaded': full_pl}
-
-# @app.get('/test')
-# def get_test():
-#   prediction = full_pl.predict(X.head(1))
-#   return {'Prediction': prediction[0]}
-
-# @app.post('/predict')
-# def post_data(data: List[float]):
-#   prediction = full_pl.predict(data)[0]
-#   if prediction == 1:
-#     return {'Prediction': 'Donner le crédit'}
-#   return {'Prediction': 'Ne pas donner le crédit'}
+@app.post('/predict')
+def post_data(data: List[float]):
+  prediction = full_pl.predict(data)[0]
+  if prediction == 1:
+    return {'Prediction': 'Donner le crédit'}
+  return {'Prediction': 'Ne pas donner le crédit'}
