6af4c70d34a9d95c7ba6faaa8859901d8fd2359d
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-03 12:50:56 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -1,22 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
-# MODEL_PATH = os.path.join("models", "lightgbm_model.joblib")
-model = joblib.load(filename="lightgbm_model.joblib")
-
 @app.get("/")
 def home():
-    return {'api_availibility': 'OK', "model_loaded": model}
+    return {'api_availibility': 'OK', "model_loaded": 'ok'}
 
-@app.get('/test')
-def get_test():
-  prediction = model.predict(X.head(1))
-  return {'Prediction': prediction[0]}
+# app = FastAPI()
+# # MODEL_PATH = os.path.join("models", "lightgbm_model.joblib")
+# model = joblib.load(filename="lightgbm_model.joblib")
 
-@app.post('/predict')
-def post_data(data: List[float]):
-  prediction = model.predict(data)[0]
-  if prediction == 1:
-    return {'Prediction': 'Donner le crédit'}
-  return {'Prediction': 'Ne pas donner le crédit'}
+# @app.get("/")
+# def home():
+#     return {'api_availibility': 'OK', "model_loaded": model}
+
+# @app.get('/test')
+# def get_test():
+#   prediction = model.predict(X.head(1))
+#   return {'Prediction': prediction[0]}
+
+# @app.post('/predict')
+# def post_data(data: List[float]):
+#   prediction = model.predict(data)[0]
+#   if prediction == 1:
+#     return {'Prediction': 'Donner le crédit'}
+#   return {'Prediction': 'Ne pas donner le crédit'}
