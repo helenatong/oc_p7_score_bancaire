a518637ca687235809e39a50e250ea7e9bbb1780
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-03 15:01:52 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -1,30 +1,22 @@
 from fastapi import FastAPI
+from typing import List
 import joblib
 
 app = FastAPI()
-
 model = joblib.load(filename="lightgbm_model.joblib")
 
 @app.get("/")
 def home():
     return {'api_availibility': 'OK_test', 'model_loaded': model}
 
-# app = FastAPI()
-# # MODEL_PATH = os.path.join("models", "lightgbm_model.joblib")
-# model = joblib.load(filename="lightgbm_model.joblib")
+@app.get('/test')
+def get_test():
+  prediction = model.predict(X.head(1))
+  return {'Prediction': prediction[0]}
 
-# @app.get("/")
-# def home():
-#     return {'api_availibility': 'OK', "model_loaded": model}
-
-# @app.get('/test')
-# def get_test():
-#   prediction = model.predict(X.head(1))
-#   return {'Prediction': prediction[0]}
-
-# @app.post('/predict')
-# def post_data(data: List[float]):
-#   prediction = model.predict(data)[0]
-#   if prediction == 1:
-#     return {'Prediction': 'Donner le crédit'}
-#   return {'Prediction': 'Ne pas donner le crédit'}
+@app.post('/predict')
+def post_data(data: List[float]):
+  prediction = model.predict(data)[0]
+  if prediction == 1:
+    return {'Prediction': 'Donner le crédit'}
+  return {'Prediction': 'Ne pas donner le crédit'}
