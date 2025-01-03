20148a9d4f3d9fe84a86ff0dc574b940c47c9bbd
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-03 12:14:43 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI
 from typing import List
 import joblib
+import uvicorn
 
 app = FastAPI()
 model_path = "/content/drive/MyDrive/openclassrooms/project_list/project_7/saved_model/lightgbm_model.joblib"
