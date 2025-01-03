96253d4efb5a8b9403f2ce3b4ca80abc7df4e6d6
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-03 16:17:44 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -2,23 +2,10 @@ from fastapi import FastAPI
 from typing import List
 import joblib
 import uvicorn
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
-import imblearn, sklearn
-from imblearn.over_sampling import SMOTE
-from imblearn.over_sampling import RandomOverSampler
-from sklearn import set_config
-from imblearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
+from lightgbm import LGBMClassifier
 
 app = FastAPI()
-full_pl = joblib.load(filename="Naive_model_no_modification.joblib")
+full_pl = joblib.load(filename="lightgbm_model.joblib")
 
 @app.get("/")
 def home():
