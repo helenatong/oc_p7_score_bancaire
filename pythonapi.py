5ebb291bb7c9f2162c5af774d35d3c79197a4c41
Helena Tong <134930919+helenatong@users.noreply.github.com>
2025-01-07 20:26:59 +0100

    Update pythonapi.py

--- pythonapi.py
+++ pythonapi.py
@@ -11,13 +11,13 @@ full_pl = joblib.load(filename="lightgbm_model.joblib")
 def home():
     return {'api_availibility': 'OK_model_loaded'}
 
-# @app.post('/predict')
-# def post_data(data: List[float]):
-#     # try:
-#     #     prediction = full_pl.predict(data)[0]
-#     # except Exception as e:
-#     #     raise HTTPException(status_code=500, detail=f"Erreur de prédiction: {e}")
-#     prediction = full_pl.predict(data)
-#     if prediction == 1:
-#         return {'Prediction': 'Donner le crédit'}
-#     return {'Prediction': 'Ne pas donner le crédit'}
+@app.post('/predict')
+def post_data(data: List[float]):
+    # try:
+    #     prediction = full_pl.predict(data)[0]
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=f"Erreur de prédiction: {e}")
+    prediction = full_pl.predict(data)
+    if prediction == 1:
+        return {'Prediction': 'Donner le crédit'}
+    return {'Prediction': 'Ne pas donner le crédit'}
